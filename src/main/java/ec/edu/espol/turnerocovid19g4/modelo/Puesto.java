29d94728ede12d236237018872d54be59f994ce0
Alejandro Paz <alejandro_paz98@hotmail.com>
2020-07-06 23:05:34 -0500

    Agregando sintomas.txt y la clase Puesto

---
+++ src/main/java/ec/edu/espol/turnerocovid19g4/modelo/Puesto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.turnerocovid19g4.modelo;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Puesto {
+    Medico medicoEncargado;
+    Paciente paciente;
+}
