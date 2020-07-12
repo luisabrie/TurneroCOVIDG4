c6c31237e928a9ad038d4634b2906707e2bb5581
Lenin Freire <lefreire@espol.edu.ec>
2020-07-12 17:03:54 -0500

    formulario doctor

--- src/main/java/ec/edu/espol/turnerocovid19g4/TertiaryController.java
+++ src/main/java/ec/edu/espol/turnerocovid19g4/TertiaryController.java
@@ -5,10 +5,67 @@
  */
 package ec.edu.espol.turnerocovid19g4;
 
+import ec.edu.espol.turnerocovid19g4.modelo.Medico;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author lfrei
  */
 public class TertiaryController {
+    
+    @FXML
+    private TextField nombreTexto;
+    
+    @FXML
+    private TextField apellidosTexto;
+    
+    @FXML
+    private TextField cedulaTexto;
+    
+    @FXML
+    private TextField especialidadTexto;
+    
+    @FXML
+    private void guardar() throws IOException{
+        String nombre=nombreTexto.getText();
+        String apellidos=apellidosTexto.getText();
+        String cedula=cedulaTexto.getText();
+        String especialidad=especialidadTexto.getText();
+        if (cedula.trim().length()>0 && nombre.trim().length()>0 && apellidos.trim().length()>0 
+                && especialidad.trim().length()>0){
+            Medico persona = new Medico(nombre,apellidos,cedula,especialidad);
+            //Se podria agregar un estado de cita para guardarlo en txt y cargarlo al cerrar sistema
+            registrarMedico(persona);
+            App.setRoot("primary");
+        }else{
+            //Muestra alerta
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Ingreso de persona");
+            alert.setContentText("Todos los campos son obligatorios");
+            alert.showAndWait();
+        }
+    };
+    
+    @FXML
+    private void cerrar() throws IOException{
+        App.setRoot("primary");
+    };
+    
+    public void registrarMedico(Medico doctor){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("src/pacientes.txt", true))){
+            String info = doctor.getNombre()+"|"+doctor.getApellido()+"|"+doctor.getCedula()+"|"+doctor.getEspecialidad();
+            bw.append(info+"\n");
+    	}catch(IOException ex) {
+    		System.out.println(ex.getMessage());
+    	}
+    }
     
 }
