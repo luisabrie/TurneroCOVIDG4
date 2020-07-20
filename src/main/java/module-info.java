4f3a9d74f9297081e60ae8ace47414683c8b5c72
Alejandro Paz <alejandro_paz98@hotmail.com>
2020-07-19 19:51:27 -0500

    nuevo pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module ec.edu.espol.turnerocovid19g4 {
     requires javafx.fxml;
     requires javafx.media;
     requires java.base;
-
+    requires com.jfoenix;
+    
     opens ec.edu.espol.turnerocovid19g4 to javafx.fxml;
     exports ec.edu.espol.turnerocovid19g4;
+
 }
